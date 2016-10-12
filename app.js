67f4c72ac8f0af03be044c6b43c511afdbdeccac
Muneeb Khan <munibkhan7737@gmail.com>
2016-10-12 03:54:39 -0700

    some changes in all files

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 var ques1 = prompt(" Question # 1 \n. What is the keyword that displays a message box? \n \n 1. alert \n \n 2. confirm \n \n 3. prompt \n ", "alert, comfirm, prompt"); 
 var ans1 = "alert";
-
 // var ques2 = prompt(" Question # 2  \n alert syntex that displays hello world. \n  \n 1. alert(\"hello world\"); \n \n 2. Alert(\"hello world\"); \n");
 // var ans2 = "alert(\"hello world\");";
 // var ques3 = prompt(" Question # 3 \n What is the keyword that declares a variable? \n  \n 1. var \n \n 2. Var \n \n 3. variable \n" , "var, Var, variable, Variable");
 // var ans3 = "var";
-var ques2 = prompt(" Question # 4 \n Following is a legal variable. \n \n var $10; \n \n 1. true \n \n 2. false \n ", "true, false");
+var ques2 = prompt(" Question # 2 \n Following is a legal variable. \n \n var $10; \n \n 1. true \n \n 2. false \n ", "true, false");
 var ans2 = "true";
-var ques3 = prompt(" Question # 5 \n var b = \"10\" - \"5\"; \n \"What will be print out?\" \n \n 1. NaN \n \n 2. undefined \n \n 3. 5 \n \n 4. notAString \n \n 5. 105 ", "NaN, undefined, notAString, 5, 105");
+var ques3 = prompt(" Question # 3 \n var b = \"10\" - \"5\"; \n \"What will be print out?\" \n \n 1. NaN \n \n 2. undefined \n \n 3. 5 \n \n 4. notAString \n \n 5. 105 ", "NaN, undefined, notAString, 5, 105");
 var ans3 = "5";
-var ques4 = prompt("Is javascript case sensitive? \n \n 1. yes \n \n 2. no", "yes, no"); 
+var ques4 = prompt("Question # 4 \n Is javascript case sensitive? \n \n 1. yes \n \n 2. no", "yes, no"); 
 var ans4 = "yes";
-var ques5 = prompt("&& and || are ___________ operators. \n \n 1. logical \n \n 2. comparison \n \n 3. mathematical", "logical, comarison, mathematical");
+var ques5 = prompt("Question # 5 \n && and || are ___________ operators. \n \n 1. logical \n \n 2. comparison \n \n 3. mathematical", "logical, comarison, mathematical");
 var ans5 = "logical";
 var ques6 = prompt(" Question # 6 \n \"!==\" This is a ______ operator. \n \n 1. logical \n \n 2. comparison \n \n 3. mathematical ");
 var ans6 = "comparison";
-var ques7 = prompt(" Question # 7 \n Using the keyword pop, you can remove the last element of an array \n \n 1. true 2. \n \n false", "true, false");
-var ans7 = true;
+var ques7 = prompt(" Question # 7 \n Using the keyword pop, you can remove the last element of an array \n \n 1. true  \n \n 2. false", "true, false");
+var ans7 = "true";
 var ques8 = prompt(" Question # 8 \n Which statement is true about slice method? \n  \n 1. The second index number specifies the last element to be copeid \n \n 2. The second number specifies the index number of the element after the last element to be copied", "1, 2");
 var ans8 = "2";   
 var ques9 = prompt(" Question # 9 \n var x = 50; \nvar y = x++; \n \n What is the value of y? \n \n 1. 50 \n \n 2. 51", "50, 51");
